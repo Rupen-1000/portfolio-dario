a9c44aca9c3c88138a0f06a76ae3d6f958d91aeb
Rupendra <rupen.shrestha11@gmail.com>
2025-09-07 21:19:48 +0200

    Finish small screen vertical home layout
    - Changed a few variable.
    - Added profile and about JSON in public for dev.
    - Added base and main styling.
    - Added major components.
    - Defined margins (styling) for the profile and content section.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
+import Layout from './components/layout/Layout.vue';
 </script>
 
-<template>
+<!-- <template>
     <div class="default_image"> 
       <img src="@/assets/under_contruction.png" alt="default image" width="100%" height="auto">
     </div>
+</template> -->
+
+<template>
+  <Layout/>
 </template>
 
 <style scoped>
---
+++ src/components/layout/profile/Profile.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="name2">
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
