5a5c70609e6ae0be958d41cb6448d2c9b71c577f
Rupendra <rupen.shrestha11@gmail.com>
2025-09-28 17:49:30 +0200

    Reposition container contract button
    - Create new component for Research text description.

---
+++ src/components/right-layout/ResearchDescription.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="
+            flex-1"
+            :class="{'mt-3':expanded}"
+    >
+        <!-- Research Title -->
+        <h2 v-katex class="
+            noto-h2 font-[450] text-shadow-sm
+            text-[clamp(1rem,1vw+0.35rem,1.25rem)]
+            md:text-[clamp(1.75rem,2vw+0.8rem,3.5rem)]
+            lg:text-[clamp(0.725rem,1vw+0.3rem,1.15rem)]
+            text-left
+            mb-2 px-1
+
+        ">
+            {{ researchItem.title }}
+        </h2>
+
+        <!-- Research Description -->
+            <!-- Expanded view -->
+        <div v-if="expanded" class="relative text-left">
+            <!-- Expanded description -->
+            <div v-for="(paragraph, idx) in researchItem.description" :key="idx"
+                class="
+                    noto-h3 font-[350]  lg:font-[400]
+                    text-[clamp(0.825rem,1vw+0.4rem,1rem)]
+                    md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
+                    lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
+                    text-shadow-md px-1 mb-2
+
+                ">
+                <p v-katex class="inline">
+                    {{ paragraph }}
+                </p>
+            </div>
+            
+            <!-- Read more: External link/s -->
+            <div>
+                <!-- Description -->
+                <span 
+                    class="
+                        noto-h3 font-[350] lg:font-[400]
+                        text-[clamp(0.825rem,1vw+0.4rem,1rem)]
+                        md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
+                        lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
+                        inline text-shadow-md px-1
+                "> 
+                    {{ researchItem.external_links.preceed_desc }}
+                </span>
+                
+                <!-- Link/s -->
+                <span
+                    v-for="(link, index) in researchItem.external_links.links"
+                    :key="index"
+                >
+                    <a :href="link" target="_blank"  rel="noopener noreferrer">
+                        <span 
+                            class="
+                                noto-h3 font-[350] lg:font-[400]
+                                text-[clamp(0.825rem,1vw+0.4rem,1rem)]
+                                md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
+                                lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
+                                inline transition-all duration-200 
+                                text-shadow-sm only-text-shadow-hover
+                        ">
+                            [{{ index+1 }}]
+                        </span>
+                    </a>
+                </span>
+            </div>
+            <!-- Revert to contracted view -->
+            <button
+                @click="emit('toggle-expand')"
+                class="
+                    noto-h3 font-[350] lg:font-[400]
+                    text-[clamp(0.825rem,1vw+0.4rem,1rem)]
+                    md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
+                    lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
+                    px-1 transition-all duration-200 
+                    text-shadow-sm only-text-shadow-hover
+            ">
+                less...
+            </button>
+        </div>
+        <!-- Contracted View -->
+        <div v-else class="relative">
+            <p v-katex class="
+                noto-h3 font-[350] lg:font-[400]
+                text-[clamp(0.825rem,1vw+0.4rem,1rem)]
+                md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
+                lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
+                text-shadow-md line-clamp-3 px-1 text-left
+                
+            ">
+                {{ researchItem.description[0] }}
+            </p>
+
+            <button
+                @click="emit('toggle-expand')"
+                class="
+                    noto-h3 font-[350] lg:font-[400]
+                    text-[clamp(0.825rem,1vw+0.4rem,1rem)]
+                    md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
+                    lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
+                    px-1 transition-all duration-200
+                    text-shadow-sm only-text-shadow-hover
+                ">
+                    more...
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+  researchItem: {
+    type: Object,
+    required: true
+  },
+  expanded: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const emit = defineEmits(['toggle-expand'])
+
+</script>
--- src/components/right-layout/ResearchLayout.vue
+++ src/components/right-layout/ResearchLayout.vue
@@ -22,85 +22,11 @@
             <!-- 
                 Container for Title and Description 
             -->
-            <div class="
-                    flex-1"
-                 :class="{'mt-3':expanded}"
-            >
-                <!-- Research Title -->
-                <h2 v-katex class="
-                    noto-h2 font-[450] text-shadow-sm
-                    text-[clamp(1rem,1vw+0.35rem,1.25rem)]
-                    md:text-[clamp(1.75rem,2vw+0.8rem,3.5rem)]
-                    lg:text-[clamp(0.725rem,1vw+0.3rem,1.15rem)]
-                    text-left
-                    mb-2 px-1
-
-                ">
-                    {{ researchItem.title }}
-                </h2>
-
-                <!-- Research Description -->
-                 <!-- Expanded view -->
-                <div v-if="expanded">
-                    <div v-for="(paragraph, idx) in researchItem.description" :key="idx"
-                        class="
-                            noto-h3 font-[350]  lg:font-[400]
-                            text-[clamp(0.825rem,1vw+0.4rem,1rem)]
-                            md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
-                            lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
-                            text-shadow-md px-1 mb-2 text-left
-
-                        ">
-                        <p v-katex class="inline">
-                            {{ paragraph }}
-                            <!-- Only show last paragraph -->
-                            <button v-if="idx === researchItem.description.length - 1"
-                                    @click="toggleExpand"
-                                    class="ml-1 transition-all duration-200 text-shadow-sm only-text-shadow-hover">
-                                less...
-                            </button>
-                        </p>
-
-                        <div v-if="idx === researchItem.description.length - 1" class="mt-2">
-                            <span class="inline"> {{ researchItem.external_links.preceed_desc }}</span>
-                            <span
-                                v-for="(link, index) in researchItem.external_links.links"
-                                :key="index"
-                                class="ml-1"
-                                >
-                                    <a :href="link" target="_blank"  rel="noopener noreferrer">
-                                        <span class="inline transition-all duration-200 text-shadow-sm only-text-shadow-hover">[{{ index+1 }}]</span>
-                                    </a>
-                                </span>
-                        </div>
-                    </div>
-                </div>
-                <!-- Contracted View -->
-                <div v-else class="relative">
-                    <p v-katex class="
-                        noto-h3 font-[350] lg:font-[400]
-                        text-[clamp(0.825rem,1vw+0.4rem,1rem)]
-                        md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
-                        lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
-                        text-shadow-md line-clamp-3 px-1 text-left
-                        
-                    ">
-                        {{ researchItem.description[0] }}
-                    </p>
-
-                    <button
-                        @click="toggleExpand"
-                        class="
-                            noto-h3 font-[350] lg:font-[400]
-                            text-[clamp(0.825rem,1vw+0.4rem,1rem)]
-                            md:text-[clamp(0.825rem,2vw+0.3rem,2rem)]
-                            lg:text-[clamp(0.625rem,1vw+0.3rem,1rem)]
-                            px-1 transition-all duration-200 text-shadow-sm only-text-shadow-hover
-                        ">
-                            more...
-                    </button>
-                </div>
-            </div>
+            <ResearchDescription
+                :researchItem="researchItem"
+                :expanded="expanded"
+                @toggle-expand="toggleExpand"
+            />
         </div>
     </div>
 </template>
@@ -108,6 +34,7 @@
 <script setup>
 import { ref } from "vue";
 import ResearchImage from "@/components/right-layout/ResearchImage.vue";
+import ResearchDescription from "@/components/right-layout/ResearchDescription.vue";
 
 defineProps({
   researchItem: {
